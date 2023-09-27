ba3669b75085bc7de310f72ee71845a5ed1a33c5
Pedro Esteves <kropinho211@outlook.com>
2023-09-27 15:29:28 -0300

    "pokemon list"

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,14 +19,17 @@ function convertPokemon(pokemon) {
 `
 }
 
+const pokemonList = document.getElementById('pokemonList')
+
 fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
+    .then((pokemons) => {
 
-        for (let i = 0; i < pokemonList.length ; i++) {
-            const pokemon = pokemonList[i];
-            console.log(convertPokemon(pokemon)) 
+        for (let i = 0; i < pokemons.length ; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemon(pokemon) 
+             
         }
         console.log(pokemonList)})
     .catch((error) => console.error(error))
