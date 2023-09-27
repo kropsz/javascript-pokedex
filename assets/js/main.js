189e232820929d9aee89b3cd68523e5f76251856
Pedro Esteves <kropinho211@outlook.com>
2023-09-27 09:44:21 -0300

    "fetch api"

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemon(pokemon) {
+    return `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                <li class="type">Grass</li>
+                <li class="type">Poison</li>
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        </div>
+    </li>
+`
+}
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+
+        for (let i = 0; i < pokemonList.length ; i++) {
+            const pokemon = pokemonList[i];
+            console.log(convertPokemon(pokemon)) 
+        }
+        console.log(pokemonList)})
+    .catch((error) => console.error(error))
+
+
