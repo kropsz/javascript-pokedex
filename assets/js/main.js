ca50a6de8ab537d09bd0f559fc15063a5c876c10
Pedro Henrique Guedes <kropinho211@outlook.com>
2023-10-02 10:05:39 -0300

    "add function map in code"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 function convertPokemon(pokemon) {
     return `
@@ -21,17 +18,11 @@ function convertPokemon(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-
-        for (let i = 0; i < pokemons.length ; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemon(pokemon) 
-             
-        }
-        console.log(pokemonList)})
-    .catch((error) => console.error(error))
+pokeAPI.getPokemons().then((pokemons = []) => {
+        const newList = pokemons.map((pokemon) => convertPokemon(pokemon))
+        const newHtml = newList.join('')
+        pokemonList.innerHTML += newHtml
+    
+    })
 
 
